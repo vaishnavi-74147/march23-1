82ff79a2ecc4e3f38bab9a663bfd3cba5d5b915d
Vaishnavi Kharde <vaishnavikharde23@gmail.com>
2023-05-23 23:52:45 +0530

    Sum.java modified

--- sum.java
+++ sum.java
@@ -2,6 +2,8 @@ class sum {
     public static void main() {
  
         System.out.print("In sum.java file");
+         System.out.print("In sum.java file");
+          System.out.print("In sum.java file");
 
     }
 }
